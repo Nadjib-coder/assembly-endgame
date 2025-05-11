58f918253dee2fe75fbe95f8d8f27ac00b02045a
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-05-11 14:20:16 +0100

    Update the project: - change the project structure for using props to export constant from between 2 components - create the game won, game lost, game over logic - style the game status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,94 @@
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import { languages } from './Languages';
+import { useState } from 'react';
+import clsx from 'clsx';
 import './App.css';
 
 function App() {
+  // State Vlaues
+  const [currentWord, setCurrentWord] = useState('react');
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  // Derived Values
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.includes(letter),
+  ).length;
+
+  const isGameWon = currentWord
+    .split('')
+    .every((letter) => guessedLetters.includes(letter));
+
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+
+  const isGameOver = isGameWon || isGameLost;
+
+  // Static Values
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const eliminations = languages.map((language, index) => {
+    const lost = index < wrongGuessCount ? 'lost' : '';
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
+    };
+    return (
+      <span key={language.name} style={styles} className={lost}>
+        {language.name}
+      </span>
+    );
+  });
+
+  const letterWord = currentWord
+    .split('')
+    .map((letter, index) => (
+      <span key={index}>
+        {guessedLetters.includes(letter) ? letter.toUpperCase() : ''}
+      </span>
+    ));
+
+  const keyboardElements = alphabet.map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
+      correct: isCorrect,
+      wrong: isWrong,
+    });
+
+    return (
+      <button
+        className={className}
+        key={letter}
+        onClick={() => handleClick(letter)}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  function handleClick(letter) {
+    setGuessedLetters((prev) => {
+      const letterSet = new Set(prev);
+      letterSet.add(letter);
+      return Array.from(letterSet);
+    });
+  }
+
   return (
-    <div className="container">
-      <Header />
-      <Main />
+    <>
+      <Header
+        isGameWon={isGameWon}
+        isGameLost={isGameLost}
+        isGameOver={isGameOver}
+      />
+      <main>
+        <section className="eliminations">{eliminations}</section>
+        <section className="letters">{letterWord}</section>
+        <section className="board">{keyboardElements}</section>
+        {isGameOver && <button className="new-game">New Game</button>}
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,34 @@
-export default function Header() {
+import clsx from 'clsx';
+
+export default function Header(props) {
+  const gameStatusClassName = clsx('game-status', {
+    won: props.isGameWon,
+    lost: props.isGameLost,
+  });
   return (
     <header>
-      <div className="title">
+      <section className="title">
         <h1>Assembly: Endgame</h1>
         <p>
           Guess the word in under 8 attempts to keep the programming world safe
           from Assembly!
         </p>
-      </div>
-      <div className="game-status">
-        <h2>You Win!</h2>
-        <p>Well done! 🎉</p>
-      </div>
+      </section>
+      <section className={gameStatusClassName}>
+        {props.isGameOver ? (
+          props.isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ) : (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : null}
+      </section>
     </header>
   );
 }
